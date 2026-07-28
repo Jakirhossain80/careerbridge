edfa5feb4a58767c9dd86595a7ebe89b873c7853
JakirHossain <jakir.devbd@gmail.com>
2026-07-29 00:02:19 +0600

    Final commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import env from "./config/env.js";
 const startServer = async () => {
   await connectDB();
 
-  app.listen(env.port, () => {
+  app.listen(env.port, "0.0.0.0", () => {
     console.log(`CareerBridge server is running on port ${env.port}`);
   });
 };
